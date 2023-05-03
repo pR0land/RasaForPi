d1755912d9eedcfd0b248122d39d977b56242886
Frederik Roland Christiansen <f.roland.c@gmail.com>
2023-05-03 14:24:59 +0200

    Har lige updated så det virker på min pc

--- RASApython/tests/test.py
+++ RASApython/tests/test.py
@@ -1,5 +1,5 @@
 ## Run this command in terminal  before executing this program
-## rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml
+##rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml
 ## and also run this in seperate terminal
 ## rasa run actions
 
