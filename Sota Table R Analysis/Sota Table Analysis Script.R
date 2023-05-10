3ef906f7142447a8272641c90e1034207bc51492
Bambalombo <92723056+Bambalombo@users.noreply.github.com>
2023-05-10 22:25:29 +0200

    Added figure and updated script

--- Sota Table R Analysis/Sota Table Analysis Script.R
+++ Sota Table R Analysis/Sota Table Analysis Script.R
@@ -16,13 +16,13 @@ save(medium_factor, complexity_factor, incentive_types_factor, time_of_payment_f
   # Remember to "Set working directory" to the right folder. Then run this:
   # ( Mark it all and press CTRL+ENTER )
 
-sota <- SOTA_Table_dots_2
+sota <- SOTA_Table_dots_
 load("factor_levels.Rdata")
-sota$Medium <- factor(SOTA_Table_dots_2$Medium, levels = medium_factor)
-sota$Complexity <- factor(SOTA_Table_dots_2$Complexity, levels = complexity_factor)
-sota$`Incentive Type` <- factor(SOTA_Table_dots_2$`Incentive Type`, levels = incentive_types_factor)
-sota$`Time of payment` <- factor(SOTA_Table_dots_2$`Time of payment`, levels = time_of_payment_factor)
-sota$Priorconcent <- factor(SOTA_Table_dots_2$Priorconcent, levels = prior_concent_factor)
+sota$Medium <- factor(SOTA_Table_dots_$Medium, levels = medium_factor)
+sota$Complexity <- factor(SOTA_Table_dots_$Complexity, levels = complexity_factor)
+sota$`Incentive Type` <- factor(SOTA_Table_dots_$`Incentive Type`, levels = incentive_types_factor)
+sota$`Time of payment` <- factor(SOTA_Table_dots_$`Time of payment`, levels = time_of_payment_factor)
+sota$Priorconcent <- factor(SOTA_Table_dots_$Priorconcent, levels = prior_concent_factor)
 
   # You dataset is now correctly formatted and called "sota".
 
@@ -30,13 +30,13 @@ sota$Priorconcent <- factor(SOTA_Table_dots_2$Priorconcent, levels = prior_conce
 
 library(ggplot2)
   # Swap out "Pages" with specific column
-empty <- which(is.na(sota$Priorconcent))
+empty <- which(is.na(sota$Items))
 sota <- sota[-empty, ]
 
-ggplot(data = sota, aes(x = Priorconcent, y = RR)) +
-  geom_boxplot() +
+ggplot(data = sota, aes(x = Items, y = RR)) +
+  geom_point() +
   stat_summary(fun.y = "mean", geom = "line", aes(group = 1)) +
-  labs(x = "Priorconcent", y = "RR")
+  labs(x = "Items", y = "RR")
 
 
 ########## PRIKDIAGRAM ##########
@@ -44,11 +44,11 @@ ggplot(data = sota, aes(x = Priorconcent, y = RR)) +
   # omdøb "XXXX" i: x = XXXX, til den kolonne du gerne vil teste på
   # husk at importere "ggplot2" og at vinge den af i packages
 
-ggplot(data = sota, aes(x = Priorconcent, y = RR)) +
-  geom_boxplot() +
+ggplot(data = sota, aes(x = Items, y = RR)) +
+  geom_point() +
   #stat_summary(fun.y = "mean", geom = "line", aes(group = 1)) +
-  labs(x = "Priorconcent", y = "RR")
-my_agg <- aggregate(sota$RR, by = list(sota$Priorconcent), FUN = function(x) c(mean = mean(x), sd = sd(x), N = length(x)))
+  labs(x = "Items", y = "RR")
+my_agg <- aggregate(sota$RR, by = list(sota$Items), FUN = function(x) c(mean = mean(x), sd = sd(x), N = length(x)))
 my_agg
 
 ########## BOKSPLOT ##########
