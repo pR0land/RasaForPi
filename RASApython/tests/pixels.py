2876afeede2250cfe03b5991f2a9ac25fe0a5f95
Frederik Roland Christiansen <f.roland.c@gmail.com>
2023-05-15 03:38:13 +0200

    Så er hele pipelinen færdig, der mangler dog stt

---
+++ RASApython/tests/pixels.py
@@ -0,0 +1,62 @@
+import apa102 as a
+
+import threading
+
+from ledPatterns import LedPattern
+from gpiozero import LED
+
+try:
+    import queue as Queue
+except ImportError:
+    import Queue as Queue
+
+class Pixels:
+    number_of_pixels = 12
+
+    def __init__(self, inPattern=LedPattern):
+
+        self.pattern = inPattern(show=self.show)
+        self.me = a.APA102(num_led=self.number_of_pixels)
+        self.power = LED(5)
+        self.power.on()
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
+        self.last_direction = None
+
+    def wakeup(self, direction=0):
+        self.last_direction = direction
+
+        def f():
+            self.pattern.wakeup(direction)
+
+        self.put(f)
+
+    def listen(self):
+        if self.last_direction:
+            def f():
+                self.pattern.wakeup(self.last_direction)
+
+            self.put(f)
+        else:
+            self.put(self.pattern.listen)
+
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
+
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
+
+    def off(self):
+        self.put(self.pattern.off)
+
+    def show(self, data):
+        for i in range(self.number_of_pixels):
+            self.me.set_pixel(i, int(data[4 * i + 1]), int(data[4 * i + 2]), int(data[4 * i + 3]))
+
+        self.me.show()
--- RASApython/tests/testSerialCommunication.py
+++ RASApython/tests/testSerialCommunication.py
@@ -3,11 +3,16 @@ import requests
 import serial
 import RPi.GPIO as GPIO
 import time
-import rasaCommunication as rc
+#import speechRecon as sr
 import pyttsx3
+
+from pixels import Pixels
 
 serialRead = True
 Talking = False
+
+
+
 
 ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
@@ -15,13 +20,42 @@ ser.baudrate=9600
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 
-engine = pyttsx3.init(driverName='sapi5') #Selve engine
-engine.setProperty("rate", 190) #Hvor hurtigt den taler
+engine = pyttsx3.init(driverName='espeak') #Selve engine
+engine.setProperty("rate", 140) #Hvor hurtigt den taler
 voices = engine.getProperty("voices")
-engine.setProperty('voice',voices[1].id) #standard windows voice (danish)
+for i,voice in enumerate(voices):
+    if voice.name == "danish":
+        engine.setProperty('voice',voices[i].id) #standard windows voice (danish)
+pixels = Pixels()
+
+
+def giveFeedback(type):
+    if type == "None":
+        pass
+    elif type == "Buzz":
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(11, GPIO.OUT)
+        ser.write(b"Start Feedback\n")
+        GPIO.cleanup()
+    elif type == "Light":
+        pixels.listen()
+
+
+def stopFeedback(type):
+    if type == "None":
+        pass
+    elif type == "Buzz":
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(11, GPIO.OUT)
+        ser.write(b"Stop Feedback\n")
+        GPIO.cleanup()
+    elif type == "Light":
+        pixels.off()
+
 
 while True:
     while serialRead:
+        print("reading serial")
         read_ser=ser.readline()
         if read_ser is not None:
             cleaned = read_ser.decode().split('\r')
@@ -34,19 +68,41 @@ while True:
                 print(alarm)
                 response = cleaned[0].split(',')[2]
                 print(response)
+                feedback = cleaned[0].split(',')[3]
+                print(feedback)
                 if alarm == "Buzz":
                     ser.write(b"Start Prompting\n")
+                elif alarm == "Light":
+                    ser.write(b"Start Prompting\n")
+                    pixels.wakeup()
 
                 if response == 'answer':
+                    message = input("Skriv hvad du vil sige:")
+                    print("stop prompt")
+                    GPIO.setmode(GPIO.BOARD)
+                    GPIO.setup(11, GPIO.OUT)
+                    ser.write(b"Stop Prompting\n")
+
+                    if alarm == "Light":
+                        pixels.off()
+                    GPIO.cleanup()
+                    Talking = True
                     serialRead = False
+
             elif state == "Talk":
+                giveFeedback(feedback)
+                print("start talking")
+                message = "Hej"
+                if alarm == "Light":
+                    pixels.off()
                 Talking = True
                 serialRead = False
     while Talking:
-        message = rc.VArecord()
 
-        r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": message})
+        print("Nu skriver vi til botten")
 
+        r = requests.post('http://192.168.0.10:5002/webhooks/rest/webhook', json={"message": message})
+        stopFeedback(feedback)
         for i in r.json():
             bot_message = i['text']
             # print(f"{self.bot_message}")
@@ -56,3 +112,7 @@ while True:
             engine.say(bot_message)  # Bot speak
             engine.runAndWait()
             engine.stop()
+        giveFeedback(feedback)
+        #message = sr.VArecord()
+       # print(message)
+        message = input("Skriv hvad du vil sige:")
