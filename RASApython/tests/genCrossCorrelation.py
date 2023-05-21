c0db6205a6bd53a126ed48c4d351f740e3f71833
Frederik Roland Christiansen <f.roland.c@gmail.com>
2023-05-21 16:01:06 +0200

    Update genCrossCorrelation.py

--- RASApython/tests/genCrossCorrelation.py
+++ RASApython/tests/genCrossCorrelation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def genCrossCorrUsingPhaseTransform(sig, refsig, fs=1, max_tau=None):
+def genCrossCorrUsingPhaseTransform(sig, refsig, fs=1, maxShiftInSec=None):
     """
     Function computing the seconds between two real signals,
     using generelized cross correlation on the phase transform, computed using numpys real fast fourier transform
@@ -9,39 +9,36 @@ def genCrossCorrUsingPhaseTransform(sig, refsig, fs=1, max_tau=None):
     :param sig:
     :param refsig:
     :param fs:
-    :param max_tau:
+    :param maxShiftInSec:
     :return The shift in seconds:
     """
     # make sure the length for the FFT is larger or equal than len(sig) + len(refsig)
     n = sig.shape[0] + refsig.shape[0]
 
-    #turns the signal and the reference signal into their respective phase transform using real fast fourier transform
-    SIG = np.fft.rfft(sig, n=n)
-    REFSIG = np.fft.rfft(refsig, n=n)
+    #turns the signal and the reference signal into their respective phasor transform using real fast fourier transform
+    phasorSig = np.fft.rfft(sig, n=n)
+    phasorRef = np.fft.rfft(refsig, n=n)
 
-    #
-    R = SIG * np.conj(REFSIG)
+    #To compute the crossSpectrum you take the phasorrepresentation and mulitply them toghether,
+    #this will multiply the magnitude of the phasors, but suptract the phase angle, therefore we instead use the
+    #conjugate of the phasor representation this makes all the angles negative instead, therefore adding the angels
+    #preserving the angle shift, a delayed signal would have. 
+    crossSpectrum = phasorSig * np.conj(phasorRef)
 
-    cc = np.fft.irfft(R / np.abs(R), n=n)
+    #normalizes the crossSpectrum between -1 and 1
+    normalizedCrossSpectrum = (crossSpectrum / np.abs(crossSpectrum))
+
+    cc = np.fft.irfft(normalizedCrossSpectrum, n=n)
 
     max_shift = int(n / 2)
-    if max_tau:
-        max_shift = np.minimum(int(fs * max_tau), max_shift)
+    if maxShiftInSec:
+        max_shift = np.minimum(int(fs * maxShiftInSec), max_shift)
 
     cc = np.concatenate((cc[-max_shift:], cc[:max_shift + 1]))
 
     # find the index where the signal from the two mics matches the best (The maximum of the generalised cross correlation)
-    shift = np.argmax(np.abs(cc)) - max_shift
+    sampleShift = np.argmax(np.abs(cc)) - max_shift
     #converts the maximum from samples to seconds using the sample frequenzy
-    tau = shift / float(fs)
+    shiftInSec = sampleShift / float(fs)
 
-    return tau, cc
-
-
-
-refsig = np.linspace(1, 10, 10)
-
-for i in range(0, 10):
-    sig = np.concatenate((np.linspace(0, 0, i), refsig, np.linspace(0, 0, 10 - i)))
-    offset, _ = gcc_phat(sig, refsig)
-    print(offset)
+    return shiftInSec
