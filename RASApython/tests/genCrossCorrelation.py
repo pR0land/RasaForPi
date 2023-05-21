2e0db156391792af11392d602ea626c89fe84840
Frederik Roland Christiansen <f.roland.c@gmail.com>
2023-05-21 22:10:02 +0200

    Update genCrossCorrelation.py

--- RASApython/tests/genCrossCorrelation.py
+++ RASApython/tests/genCrossCorrelation.py
@@ -28,16 +28,30 @@ def genCrossCorrUsingPhaseTransform(sig, refsig, fs=1, maxShiftInSec=None):
     #normalizes the crossSpectrum between -1 and 1
     normalizedCrossSpectrum = (crossSpectrum / np.abs(crossSpectrum))
 
+    #reverts back to sample representation of signal using the invers real fast fourier transform
     cc = np.fft.irfft(normalizedCrossSpectrum, n=n)
 
-    max_shift = int(n / 2)
+    # we then make calculate how much the maximum shift can be, either the mean of the signals, or the maximum time
+    # in seconds as times the sample rate
+    maxShift = int(n / 2)
     if maxShiftInSec:
-        max_shift = np.minimum(int(fs * maxShiftInSec), max_shift)
+        maxShift = np.minimum(int(fs * maxShiftInSec), maxShift)
 
-    cc = np.concatenate((cc[-max_shift:], cc[:max_shift + 1]))
+    # we then only take the last part of the signal, and the first part of the signal
+    # this is done because the shift cant be longer than maxshift, since the mics are only that far appart
+    # the reason we take the last part of the signal first, is because these represent the negative lag
+    # ei the lag where the refference signal is the one leading the input signal meaning the refference mic is the one
+    # getting the signal first.
+    # The other half is the positive lag, representing the situation where the input signal is the one which captures
+    # the signal first.
+    cc = np.concatenate((cc[-maxShift:], cc[:maxShift + 1]))
 
     # find the index where the signal from the two mics matches the best (The maximum of the generalised cross correlation)
-    sampleShift = np.argmax(np.abs(cc)) - max_shift
+    # then subtract the maximum shift, since if the lag was from the second part of the signal, the lag is positive
+    # but as the negative lag is saved in the same array we need to subtrack those indexes
+    # on the other hand, if the lag was in the first part of the signal, the lag should be negative, duo to the mics
+    # being shifted, so its the refference mic capturing the signal last.
+    sampleShift = np.argmax(np.abs(cc)) - maxShift
     #converts the maximum from samples to seconds using the sample frequenzy
     shiftInSec = sampleShift / float(fs)
 
